b04b45a0059b0334a00418c9c28d5d1dbca8bee1
ggreen1980 <74391414+ggreen1980@users.noreply.github.com>
2026-07-03 21:34:09 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// ========================================================
+// REPO CONFIGURATION & CLOUD APIS
+// ========================================================
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbxPoyWNVcpBaFSJmTm1BoZTd01EfouS13emZPQ2lJvCK5MyW8zMlgQ-0LyYCWU85KFjOw/exec";
+
+const CORRECT_PASSCODE = "1234";
+const CLOUDINARY_CLOUD_NAME = "xzpkydjm";
+const CLOUDINARY_PRESET = "keychain_preset";
